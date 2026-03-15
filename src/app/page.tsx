13b60bad5d43cc7b679bfb9903efbe5ef3a411c9
a1919727 <a1919727@adelaide.edu.au>
2026-03-15 11:28:55 +1030

    add title and all movies button for popular section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getNowPlayingMovies, getTmdbImageUrl } from "@/lib/tmdb";
 import { MovieCard } from "@/components/movie-card";
 import { HeroCarousel } from "@/components/hero-carousel";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function HomePage() {
   const movieResponse = await getNowPlayingMovies();
@@ -17,11 +19,26 @@ export default async function HomePage() {
     <main className="mx-auto w-full max-w-7xl px-6 py-10">
       <HeroCarousel movies={heroMovies} />
 
-      <div className="mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <section>
+        <h2 className="text-2xl font-bold text-foreground md:text-3xl mt-10">
+          Popular
+        </h2>
+        <div className=" relative mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+        <div className="flex justify-end mt-10">
+          <Button
+            asChild
+            variant="outline"
+            size="lg"
+            className="text-foreground right-0 px-10"
+          >
+            <Link href="/movies"> All movies </Link>
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
